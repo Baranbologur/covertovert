26a0e7a32221a963a74ea40b9aac428c0acd955a
yunus emre keleş <116894704+yekeless@users.noreply.github.com>
2024-11-05 10:03:42 +0300

    Update icmp_receiver.py

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,15 @@
 import scapy
 
+
+from scapy.all import sniff, IP, ICMP
+
+def ttl_filter(frame):
+    if frame.haslayer(ICMP) and frame[IP].ttl == 1:
+        return True
+    return False
+
+if __name__ == "__main__":
+    packets = sniff(count=1, lfilter=ttl_filter)
+    if packets:
+        packets[0].show()
 # Implement your ICMP receiver here
